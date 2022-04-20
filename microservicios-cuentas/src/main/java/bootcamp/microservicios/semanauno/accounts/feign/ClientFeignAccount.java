03a9a503940a4b5f468d0d5c2d8959b8d8403470
Lulio1981 <63922659+Lulio1981@users.noreply.github.com>
2022-04-20 17:42:26 -0500

    Delete microservicios-cuentas directory

--- microservicios-cuentas/src/main/java/bootcamp/microservicios/semanauno/accounts/feign/ClientFeignAccount.java
+++
@@ -1,8 +0,0 @@
-package bootcamp.microservicios.semanauno.accounts.feign;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(name = "microservice-company-account-types")
-public interface ClientFeignAccount {
-
-}
